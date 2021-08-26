131f9ea111ca0cc756348e480ab9d8368b5d6c34
Md Tawfiqul Hasan Khan <tawfiqul.hasankhan@gmail.com>
2021-08-26 00:29:07 -0400

    add tests for login

--- test/user-auth.test.js
+++ test/user-auth.test.js
@@ -3,19 +3,25 @@ const app = require("../app");
 const mongoose = require("mongoose");
 const User = require("../models/User");
 const request = require("supertest");
-const { deleteOne } = require("../models/User");
 
-describe("Connection", () => {
-  beforeEach((done) => {
-    mongoose.connect(
-      process.env.MONGO_URI,
-      { useNewUrlParser: true, useUnifiedTopology: true },
-      () => done()
-    );
-  });
+beforeEach((done) => {
+  mongoose.connect(
+    process.env.MONGO_URI,
+    { useNewUrlParser: true, useUnifiedTopology: true },
+    () => done()
+  );
+});
 
+afterEach((done) => {
+  mongoose.disconnect();
+  done();
+});
+
+jest.useRealTimers();
+
+describe("User Registration and login", () => {
   test("POST/auth/register", async () => {
-    const username = "jest-test";
+    const username = "jest-register";
     await request(app)
       .post("/auth/register")
       .send({ username, password: "test12345" })
@@ -25,6 +31,7 @@ describe("Connection", () => {
         expect(response.body.user).toBeTruthy();
         expect(response.body.user.username).toBe(username);
       });
+
     // check Registration with existing username
     await request(app)
       .post("/auth/register")
@@ -38,9 +45,55 @@ describe("Connection", () => {
       });
     // delete the newly created user
     await User.deleteOne({ username });
+
+    //if username, password is not provided
+    await request(app)
+      .post("/auth/register")
+      .send({})
+      .expect(400)
+      .then((response) => {
+        expect(Array.isArray(response.body.errors)).toBeTruthy();
+        expect(response.body.errors.length).toEqual(2);
+      });
   });
-  afterEach((done) => {
-    mongoose.disconnect();
-    done();
+
+  test("POST/auth/login", async () => {
+    const username = "jest-login";
+    await User.create({
+      username,
+      password: "test12345",
+    });
+    await request(app)
+      .post("/auth/login")
+      .send({ username, password: "test12345" })
+      .expect(200)
+      .then((response) => {
+        // Check returned data
+        expect(response.body.user).toBeTruthy();
+        expect(response.body.user.username).toBe(username);
+      });
+
+    // delete the newly created user
+    await User.deleteOne({ username });
+
+    //if password is not provided
+    await request(app)
+      .post("/auth/login")
+      .send({ username })
+      .expect(400)
+      .then((response) => {
+        expect(Array.isArray(response.body.errors)).toBeTruthy();
+        expect(response.body.errors.length).toEqual(1);
+      });
+
+    // if wrong credentials
+    await request(app)
+      .post("/auth/login")
+      .send({ username: "wrong", password: "wrong" })
+      .expect(401)
+      .then((response) => {
+        expect(response.body.error).toBeTruthy();
+        expect(response.body.error).toBe("Invalid username or password");
+      });
   });
 });
